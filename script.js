167d2218ba71d2146aec9c62f03ca2972acd53dd
alerai1994 <ale.raimondo@gmail.com>
2026-02-08 13:18:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const coins = [
+    "bitcoin",
+    "ethereum",
+    "binancecoin",
+    "solana",
+    "ore-network",
+    "raydium"
+];
 
+async function getPrices() {
+    try {
+        const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(
+            ","
+        )}&vs_currencies=usd`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        coins.forEach(coin => {
+            const priceElement = document.querySelector(
+                `#${coin} .price`
+            );
+            priceElement.textContent = `$${data[coin].usd}`;
+        });
+
+    } catch (error) {
+        console.error("Errore nel recupero prezzi", error);
+    }
+}
+
+// primo caricamento
+getPrices();
+
+// aggiornamento ogni 10 secondi
+setInterval(getPrices, 10000);
